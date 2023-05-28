b68edb10047e307cef2dc635100e0f70beee7de0
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-05-27 22:32:54 -0600

    aplicacion terminada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
+import { Geolocation } from '@capacitor/geolocation';
+import { ModalController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,80 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map')mapRef!: ElementRef<HTMLElement>;
+  map!: GoogleMap;
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
 
+  ionViewDidEnter() {
+    // Probably not necessary in the future
+    setTimeout(() => {
+      this.createMap();
+    }, 200);
+  }
+
+  async createMap() {
+    this.map = await GoogleMap.create({
+      forceCreate: true, // Prevent issues with live reload
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: 20.03133190324908,
+          lng: -100.71976903255897,
+        },
+        zoom: 13,
+      },
+    });
+    await this.addMarkers();
+  }
+
+  async addMarkers() {
+    const markers: Marker[] = [
+      {
+        coordinate: {
+          lat: 20.026746469231476,
+          lng: -100.70026227288871,
+        },
+        title: 'cerro del toro',
+        snippet: 'ahi subo a hacer ejercicio',
+      },
+      {
+        coordinate: {
+          lat: 20.024652910396284,
+          lng: -100.72651829683636,
+        },
+        title: 'Panteon MPAL Acambaro',
+        snippet: 'ahi esta la floreria',
+      },
+    ];
+    await this.map.addMarkers(markers);
+
+    this.map.setOnMarkerClickListener(async (marker) => {
+      const modal = await this.modalCtrl.create({
+        component: ModalPage,
+        componentProps: {
+          marker,
+        },
+        breakpoints: [0, 0.3],
+        initialBreakpoint: 0.3,
+      });
+      modal.present();
+    });
+  }
+
+  async locateMe() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    if (coordinates) {
+      this.map.setCamera({
+        coordinate: {
+          lat: coordinates.coords.latitude,
+          lng: coordinates.coords.longitude,
+        },
+        zoom: 17,
+      });
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKey: 'AIzaSyBZZg9sLizL-qMevBTiZp608zw3Ycwz7sU'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
